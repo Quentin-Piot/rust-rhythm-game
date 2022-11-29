4b9fb90cdcfb14732f464ab989f4bf0e039e2279
Quentin <qkpiot@gmail.com>
2022-11-29 18:40:42 +0800

    start CI and web assembly exportation

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,15 @@ mod consts;
 mod types;
 
 use arrows::ArrowsPlugin;
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 
 use bevy::prelude::*;
 use bevy::window::{close_on_esc, PresentMode};
 use bevy_editor_pls::prelude::*;
-use wasm_bindgen::prelude::*;
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 
-#[wasm_bindgen]
 fn main() {
     let config = types::load_config();
 
@@ -21,8 +20,8 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
                 title: "Rust Rhythm Game".to_string(),
-                width: 900.,
-                height: 600.,
+                width: 1280.,
+                height: 720.,
                 present_mode: PresentMode::AutoVsync,
                 resizable: false,
                 ..default()
@@ -32,6 +31,7 @@ fn main() {
         .add_system(close_on_esc)
         .add_plugin(EditorPlugin)
         .add_plugin(ArrowsPlugin)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(spawn_camera)
         .insert_resource(config)
         .run();
