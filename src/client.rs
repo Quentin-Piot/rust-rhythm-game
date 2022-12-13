3b4239784d2908984e299c90cc11cc371fa53883
Quentin Piot <qkpiot@gmail.com>
2022-12-13 17:07:25 +0800

    fix wasm issues

--- src/client.rs
+++ src/client.rs
@@ -2,13 +2,13 @@ use bevy::log::info;
 use std::fs::File;
 use std::io;
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 pub async fn get_song() -> File {
     // chaining .await will yield our query result
 
     info!("Downloading file");
 
-    let resp = reqwest::get("https://storage.googleapis.com/game-storage-qp/territory1.toml")
+    let resp = reqwest::get("https://storage.googleapis.com/game-storage-qp/territory2.toml")
         .await
         .unwrap();
     let body = resp.text().await.unwrap();
